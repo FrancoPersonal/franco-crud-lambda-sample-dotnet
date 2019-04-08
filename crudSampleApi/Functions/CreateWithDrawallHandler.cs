3a607cdd7a795213662fbb5e1be81652b702f8a0
jose <jafr6@hotmail.com>
2019-04-08 02:32:54 -0500

    Basic Example Get with web api

---
+++ crudSampleApi/Functions/CreateWithDrawallHandler.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpHandlers.Functions
+{
+    
+    public class CreateWithDrawallHandler
+    {
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task<APIGatewayProxyResponse> Run(APIGatewayProxyRequest request)
+        {
+            // var requestModel = JsonConvert.DeserializeObject<UpdateItemRequest>(request.Body);
+
+            // var result = await mediator.Send(requestModel);var 
+
+            var result = await Task.Run(() => { return "Hello Get"; });
+
+            return new APIGatewayProxyResponse { StatusCode = 200, Body = JsonConvert.SerializeObject(result) };
+        }
+
+
+
+    }
+}
